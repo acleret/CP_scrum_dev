7e6f420ae5750b1fa03835d0edd4898979826e02
Nathalie Craeye <nathalie.craeye@laposte.net>
2016-11-18 07:13:06 +0100

    Ajout mineur : page du site cdp

--- src/web/structure.php
+++ src/web/structure.php
@@ -32,7 +32,7 @@ class Structure {
               <span class="icon-bar"></span>
               <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="index.php">CdP</a>
+            <a class="navbar-brand" href="cdp.php">CdP</a>
           </div>
           <div class="collapse navbar-collapse" id="myNavbar">
             <ul class="nav navbar-nav">
