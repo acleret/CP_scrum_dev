1d9dd04d65bd9dcbb9ed4a9f70d4b897a57625c6
Thomas VIGUÉ <thomas.vigue@etu.u-bordeaux.fr>
2016-12-06 13:26:41 +0100

    bug fix

--- src/web/modificationProjet.php
+++ src/web/modificationProjet.php
@@ -7,14 +7,14 @@ if (isset($_SESSION["session"])) {
 		$client = $_POST["client"];
 		$description = htmlspecialchars($_POST["descr"], ENT_QUOTES);
 	}
-	
+
 	if (isset($_POST["idPO"]))
 		$id_PO = $_POST["idPO"];
 	else
 		$id_PO = $_SESSION["id_co"];
-	
+
 	if (isset($_GET["action"]) && $_GET["action"]=="ajouter") {
-        
+
 		//$devs = array(0 => $_SESSION['id_co'], 1 => 2, 2 => 3);
 		$id_projet = $db->ajouterProjetBDD($nom, $client, $description, $_POST['PO'], $_POST['SM'], $_POST['devs']);
 		$expire = time() + 60 * 60 * 24; // 24 heures
@@ -25,14 +25,16 @@ if (isset($_SESSION["session"])) {
 	else if (isset($_GET["action"]) && $_GET["action"]=="éditer") {
 		if(isset($_POST['PO'])) $po = $_POST['PO'];// : $po = null;
 		if(isset($_POST['SM'])) $sm = $_POST['SM'];// : $sm = null;
-		
+
 		// depuis le bouton "Modifier" de listeProjets.php
-		if (isset($_POST["idProjet"]) && isset($_POST["pageActuelle"])) { 
+		echo "string<br />\n".$_POST["idProjet"];
+		if (isset($_POST["idProjet"])) {
+			echo "\n".$_POST["idProjet"]."<br />\n|<br />\n".$description;
 			if ($db->modifierProjetBDD($_POST["idProjet"], $nom, $client, $description, $po, $sm, $_POST['devs'])) {
-				header("Location: listeProjets.php?page=".$_POST["pageActuelle"]);
+				header("Location: listeProjets.php");
 				exit();
 			}
-		} 
+		}
 		// depuis le bouton "Modifier" de projet.php
 		else {
 			if ($db->modifierProjetBDD($_COOKIE["id_projet"], $nom, $client, $description, $po, $sm, $_POST['devs'])) {
